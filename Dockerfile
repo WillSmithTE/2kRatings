338daa76fe297043e66c6de4f48a58a72a9ffeb9
Will Smith <willsmithte@gmail.com>
2020-03-29 18:38:15 +1100

    cut the db stuff out just store the thing as a python dict

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /usr/src/app
 
 RUN apt-get update && apt-get install -y python3 python3-pip
 RUN apt-get -y install python3.7-dev
-# RUN apt-get install postgresql-server-dev-10 gcc python3-dev musl-dev
-# RUN pip3 install psycopg2
+RUN apt-get -y install postgresql-server-dev-10 gcc python3-dev musl-dev
+RUN pip3 install psycopg2
 
 COPY requirements.txt ./
 
@@ -13,8 +13,8 @@ RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-# USER postgres
-# EXPOSE 5432
+USER postgres
+# EXPOSE 2345
 # VOLUME ["/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql"]
 
 CMD [ "python3", "./main.py" ]
--- main.py
+++ main.py
@@ -3,22 +3,16 @@ import urllib.request
 import time
 import os
 import json
+import psycopg2
 from bs4 import BeautifulSoup
 from teams import teams
 from inflection import parameterize
 from flask import Flask, request, redirect, url_for, flash, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+import logging
 
 BASE_RATINGS_ADDRESS = 'https://www.2kratings.com/nba2k20-team/'
-
-POSTGRES_CONFIG = {
-	'user': 'nbaratings',
-	'pw': 'nbaratings',
-	'db': 'nbaratings',
-	'host': 'localhost',
-	'port': '5432'
-}
+players = {}
 
 def kebabToSentence(kebabCaseName):
 	return ' '.join(kebabCaseName.split('-'))
@@ -27,35 +21,26 @@ def getTeamUrl(teamName):
 	kebabCaseName = parameterize(teamName)
 	return BASE_RATINGS_ADDRESS + kebabCaseName
 
-def getScoresAndPlayers():
-	data = {}
+def setPlayers():
 	for team in teams:
 		teamName = team['teamName']
-		teamData = {}
 		teamUrl = getTeamUrl(teamName)
 		response = requests.get(teamUrl)
 		soup = BeautifulSoup(response.text, 'html.parser')
 		playersTags = soup.find_all('td', class_='roster-entry')
-		players = list(map(lambda x: x.contents[0].contents[1], playersTags))
+		playersForTeam = list(map(lambda x: x.contents[0].contents[1].lower(), playersTags))
 		scoresTags = soup.find_all('span', class_='roster-rating')[::3]
 		scores = list(map(lambda x: int(x.contents[0]), scoresTags))
-		for i in range(len(players)):
-			teamData[players[i]] = scores[i]
-		data[teamName] = teamData
-	return data
+		for i in range(len(playersForTeam)):
+			players[playersForTeam[i]] = { 'team': teamName, 'rating': scores[i] }
 
 app = Flask(__name__)
 CORS(app)
-
-db = SQLAlchemy()
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES_CONFIG
-
-db.init_app(app)
+setPlayers()
 
 @app.route('/api/2k/<name>', methods=['GET'])
 def getPrediction(name):
-    return json.dumps(kebabToSentence(name))
+    return json.dumps(players[kebabToSentence(name)])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
